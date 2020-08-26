eccdd30b951d10737ce6755e3cfd1c06682ec02a
veenabaraik <61896907+veenabaraik@users.noreply.github.com>
2020-08-26 15:05:20 +0530

    Add files via upload

---
+++ assets/js/data.js
@@ -0,0 +1,31 @@
+var mentor_total = 3;
+var mentor_name = [
+    "Devansh Shrivastava",
+    "Anuj Gangwal",
+    "Gaurav Chouhan"
+];
+var mentor_desc = "Co-founder at TYP";
+var mentor_social = "<a href=\"#\"><span class=\"social facebook\"></span></a>";
+mentor_social += "<a href=\"#\"><span class=\"social linkedin\"></span></a>";
+mentor_social += "<a href=\"#\"><span class=\"social twitter\"></span></a>";
+var mentor_avatar = [
+    "assets/avatar/mentors/TYP-DevanshSir.jpg",
+    "assets/avatar/members/avatar.png",
+    "assets/avatar/mentors/TYP-GauravSir.jpg"
+];
+
+var member_total = 7;
+var member_name = [
+    "Member 01",
+    "Member 02",
+    "Member 03",
+    "Member 04",
+    "Member 05",
+    "Member 06",
+    "Member 07",
+];
+var member_desc = "Team member at TYP";
+var member_social = "<a href=\"#\"><span class=\"social facebook\"></span></a>";
+member_social += "<a href=\"#\"><span class=\"social linkedin\"></span></a>";
+member_social += "<a href=\"#\"><span class=\"social twitter\"></span></a>";
+var member_avatar = "assets/avatar/members/avatar.png";
